dc1f0b8daf296f023438fa62b3e725908986efda
Zakaria Mourtaban <zakariamourtaban@gmail.com>
2025-01-29 14:16:43 +0200

    created controller

---
+++ app/Http/Controllers/GithubController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GithubController extends Controller
+{
+    function GetOldPrs(){}
+    function GetOpenPrsWithReview(){}
+    function GetOpenPrsWithSuccess(){}
+    function GetOpenPrsWithoutReviews(){}
+    function LoopThroughList(){}
+    
+}
