135ae031abbf89f4579762597e176b24d47e08e4
Zakaria Mourtaban <zakariamourtaban@gmail.com>
2025-01-29 14:36:18 +0200

    added documentation

--- app/Http/Controllers/GithubController.php
+++ app/Http/Controllers/GithubController.php
@@ -6,10 +6,22 @@ use Illuminate\Http\Request;
 
 class GithubController extends Controller
 {
-    function GetOldPrs(){}
-    function GetOpenPrsWithReview(){}
-    function GetOpenPrsWithSuccess(){}
-    function GetOpenPrsWithoutReviews(){}
+    // file naming will be as #ofPrs-FileTitle.txt
+    // files will be in csv format as PR#, PR Title, PR Url
+
+    // gets prs older than 14 days and writes them to file
+    function GetOldPrs($repo){}
+
+    // gets prs with review required
+    function GetOpenPrsWithReview($repo){}
+
+    // gets prs where review status is success
+    function GetOpenPrsWithSuccess($repo){}
+
+    // get prs without reviews
+    function GetOpenPrsWithoutReviews($repo){}
+
+
     function LoopThroughList(){}
     
 }
